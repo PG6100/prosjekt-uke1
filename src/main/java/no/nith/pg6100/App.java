5029682a1b7caa4879ea6a4b54de2fbb7b45da60
Naimdjon Takhirov <naimdjon@gmail.com>
2014-01-19 21:44:03 +0100

    Initial import

---
+++ src/main/java/no/nith/pg6100/App.java
@@ -0,0 +1,13 @@
+package no.nith.pg6100;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
