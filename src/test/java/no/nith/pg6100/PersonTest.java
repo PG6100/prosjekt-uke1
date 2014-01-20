0cb2b6cf012f48b899f7d134e4daa2af6f553c06
Naimdjon Takhirov <naimdjon@gmail.com>
2014-01-20 16:24:42 +0100

    eksempel test case

--- src/test/java/no/nith/pg6100/PersonTest.java
+++ src/test/java/no/nith/pg6100/PersonTest.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 
 import javax.persistence.EntityManager;
 
+import static org.junit.Assert.assertEquals;
+
 public class PersonTest{
         private EntityManager em;
 
         @Before
         public void beforeEach(){
-            //TODO
-            em = null;
+            em=javax.persistence.Persistence.createEntityManagerFactory("PG6100").createEntityManager();
         }
 
         @After
@@ -20,11 +21,22 @@ public class PersonTest{
             em.close();
         }
 
-        @Test
-        public void testLagring(){
 
-        }
 
+    @Test
+    public void testLagring(){
+        em.getTransaction().begin();
+        Person person = new Person();
+        person.setEpost("test1@test.no");
+        person.setFornavn("Ola");
+        person.setEtternavn("Nordmann");
+        em.persist(person);
+        em.flush();
+        Person p=(Person) em.createQuery("from Person p where p.epost=?1").setParameter(1,"test1@test.no").getSingleResult();
+        assertEquals("Ola", p.getFornavn());
+        assertEquals("Nordmann", p.getEtternavn());
+        em.getTransaction().commit();
+    }
 
 
 }
